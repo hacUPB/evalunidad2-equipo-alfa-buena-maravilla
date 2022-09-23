e115b0577826f0690080a05621170033b33039be
Teotsky1 <mateo.arangog@upb.edu.co>
2022-09-22 22:49:31 -0500

    pull

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,7 +5,11 @@
 
 EventList *CreateEventList(void)
 {
-
+    EventList   *eventList=malloc(sizeof(eventList));
+    eventList->isEmpty=1;
+    eventList->head=NULL;
+    eventList->last=NULL;
+    return eventList;
 }
 
 void DestroyEventList(EventList *this)
@@ -20,7 +24,19 @@ Event *SearchEvent(EventList *this, char *name)
 
 void AddEvent(EventList *this, Event *event)
 {
+    if (this.isEmpty==1)
+    {
+        this.isEmpty=0;
+        this->head=event;
+        this->last=event;
+        event->next=NULL;
 
+    }
+    else
+    {
+        this->last->next=event;
+        this-> last=next;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
@@ -29,5 +45,12 @@ void RemoveEvent(EventList *this, char *name)
 
 void ListEvents(EventList *this)
 {
-
+    if(this->isEmpty==1)
+    {
+     printf("Empty\n");   
+    }
+    else 
+    {
+        printf("%s\n",this->head->eventName);
+    }
 }
