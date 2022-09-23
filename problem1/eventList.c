ee024a12f6a541d52bf783bce38c2810933c66d5
Teotsky1 <mateo.arangog@upb.edu.co>
2022-09-23 01:12:26 -0500

    Cmabio

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -6,11 +6,11 @@
 EventList *CreateEventList(void)
 {
 
-EventList *eventlist = malloc(sizeof(EventList));
-eventlist->isEmpty=1;
-eventlist->head=NULL;
-eventlist->last=NULL;
-return eventlist;
+    EventList *eventlist = malloc(sizeof(EventList));
+    eventlist->isEmpty=1;
+    eventlist->head=NULL;
+    eventlist->last=NULL;
+    return eventlist;
 
 }
 
@@ -33,51 +33,47 @@ void AddEvent(EventList *this, Event *event)
         this->last=event;
         event->next=NULL;
 
-if (this->isEmpty==1)
-{
-  this->isEmpty=0;
-  this->head=event;
-  this->last=event;
-  event->next=NULL;
-}
+            if (this->isEmpty==1)
+            {
+                this->isEmpty=0;
+                this->head=event;
+                this->last=event;
+                event->next=NULL;
+            }
 
-else
-{
-    this->last->next=event;
-    this->last=event;
-}
+            else
+            {
+             this->last->next=event;
+             this->last=event;
+            }
+        }
+    }
+    void RemoveEvent(EventList *this, char *name)
+    {
 
-}
-
-void RemoveEvent(EventList *this, char *name)
-{
-}
-
-void ListEvents(EventList *this)
-{
-    int i=1; 
-
-  
-   if (this->isEmpty==1)
-   {
-    printf("empty\n");
-   }
-   else
-   { 
-     printf("%s\n",this->head->eventName);
-
-     while (i!=0)
-     {
-      if((this->head+i)->next==NULL)
-      {
-        i=0;
-         printf("%s\n",(this->head+i)->eventName);
-      }
-      else
-      {
-        printf("a");
-        printf("%s\n",(this->head+i)->eventName);
-      }
-     }
-     
-   }
+    }
+    void ListEvents(EventList *this)
+    {
+        int i=1; 
+            if (this->isEmpty==1)
+            {
+                printf("empty\n");
+            }
+            else
+            { 
+                  printf("%s\n",this->head->eventName);
+                     while (i!=0)
+                    {
+                        if((this->head+i)->next==NULL)
+                        {
+                            i=0;
+                            printf("%s\n",(this->head+i)->eventName);
+                        }
+                        else
+                        {
+                            printf("a");
+                            printf("%s\n",(this->head+i)->eventName);
+                        }
+                     } 
+            }
+    }
